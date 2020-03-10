ddcb00566413e77e8ea8cca4bda547db7017b467
patiwat <patiwat.phewlaong@pranworks.com>
2020-03-10 19:36:02 +0700

    set launch port

--- MJU.DataCenter.Person/Startup.cs
+++ MJU.DataCenter.Person/Startup.cs
@@ -45,7 +45,7 @@ namespace MJU.DataCenter.Person
             }
 
             app.UseCors(builder =>
-            builder.WithOrigins("https://localhost:5003"));
+            builder.WithOrigins("https://localhost:5002"));
 
             app.UseSwagger();
 
