575624625d2d9c087a4fc5d1b71f5adb5aeb6b5e
cnxdevsoftbe <patiwat.phewlaong@pranworks.com>
2020-03-21 21:18:53 +0700

    add

---
+++ MJU.DataCenter.ResearchExtension/ViewModels/ResearchDataViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MJU.DataCenter.ResearchExtension.ViewModels
+{
+    public class ResearchDataViewModel
+    {
+        public int? MoneyTypeId { get; set; }
+        public string MoneyTypeName { get; set; }
+        public List<ResearchData> ResearchData { get; set; }
+    }
+    public class ResearchData
+    {
+        public int? ResearchId { get; set; }
+        public string ResearchNameEN { get; set; }
+        public string ResearchNameTH { get; set; }
+    }
+}
