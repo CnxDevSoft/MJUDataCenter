6374fc5e8bd71fa6a5e9ee8bf2caf95a79f3c208
CnxDevSoft <33708980+aunsparrow@users.noreply.github.com>
2020-03-21 15:52:09 +0700

    update view

---
+++ MJU.DataCenter.ResearchExtension/Models/DC_ResearchPaperPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MJU.DataCenter.ResearchExtension.Models
+{
+    public class DC_ResearchPaperPerson
+    {
+        public int? paper_ID { get; set; }
+        public int? personID { get; set; }
+        public decimal? paperPercent { get; set; }
+        public int? personGroupID { get; set; }
+        public string personGroupName { get; set; }
+    }
+}
---
+++ MJU.DataCenter.ResearchExtension/Models/DC_Researcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MJU.DataCenter.ResearchExtension.Models
+{
+    public class DC_Researcher
+    {
+        public int? personID { get; set; }
+        public string prefixNameTH { get; set; }
+        public string nameTH { get; set; }
+        public string sirNameTH { get; set; }
+        public int? personGroupID { get; set; }
+        public int? departmentID { get; set; }
+        public int? departmentCode { get; set; }
+        public string departmentNameTH { get; set; }
+        public int? research_id { get; set; }
+        public decimal? research_work_percent { get; set; }
+        public decimal? personMoney { get; set; }
+    }
+}
