534bd2ff4c32067aad14ca40d2ba50e0dd148ec3
CnxDevSoft <33708980+aunsparrow@users.noreply.github.com>
2020-03-21 21:22:27 +0700

    update apichai graph

---
+++ MJU.DataCenter.ResearchExtension/ViewModels/RankResearchMoney.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MJU.DataCenter.ResearchExtension.ViewModels
+{
+    public class RankResearchMoney
+    {
+        public string RankResearchName { get; set; }
+        public int? Money { get; set; }
+    }
+    public class RankResearchMoneyViewModel
+    {
+        public List<RankResearchMoney> RankResearchMoney { get; set; }
+    }
+
+}
